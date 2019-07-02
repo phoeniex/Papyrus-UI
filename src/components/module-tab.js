3ee7d5cf127a4444214f07c123cee5cd406ea1ac
Rawipon Srivibha <rawipon.srivibha@allianz.com>
2019-07-02 19:18:32 +0700

    Update Tab

--- src/components/module-tab.js
+++ src/components/module-tab.js
@@ -1,70 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 
-const StyledTabs = withStyles({
+const ModuleTabs = withStyles({
   indicator: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-    borderBottom: '1px dash #1CA7EC',
-    '& > div': {
-      width: '100%',
-      color: '#1CA7EC'
-    },
+    backgroundColor: 'transparent'
   },
-})(props => <Tabs {...props} TabIndicatorProps={{ children: <div /> }} />);
+})(props => <Tabs {...props} />);
 
-const StyledTab = withStyles(theme => ({
+const ModuleTab = withStyles(theme => ({
   root: {
     textTransform: 'none',
     color: '#2F2F2F',
-    opacity: 0.6,
     fontWeight: theme.typography.fontWeightRegular,
-    fontSize: 14,
+    fontSize: theme.typography.pxToRem(14),
     marginRight: theme.spacing(1),
+    opacity: 0.6,
     '&:hover': {
-      color: '#2F2F2F',
       opacity: 1,
     },
     '&$selected': {
-      color: '#2F2F2F',
       fontWeight: theme.typography.fontWeightMedium,
-      opacity: 1,
-    },
-    '&:focus': {
-      color: '#2F2F2F',
+      textDecoration: 'underline dashed #1CA7EC',
       opacity: 1,
     },
   },
+  selected: {},
 }))(props => <Tab disableRipple {...props} />);
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-  },
-  typography: {
-    padding: theme.spacing(3),
-  },
+    flexGrow: 1
+  }
 }));
 
-function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  );
-}
-
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default function ModuleTab() {
+export default function CustomizedTabs() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -74,36 +45,24 @@ export default function ModuleTab() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <StyledTabs
+        <ModuleTabs
           value={value}
           onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto">
-          <StyledTab label="Item One" />
-          <StyledTab label="Item Two" />
-          <StyledTab label="Item Three" />
-          <StyledTab label="Item Four" />
-          <StyledTab label="Item Five" />
-          <StyledTab label="Item Six" />
-          <StyledTab label="Item Seven" />
-          <StyledTab label="Item Eight" />
-          <StyledTab label="Item Nine" />
-          <StyledTab label="Item Ten" />
-        </StyledTabs>
-      </AppBar>
-      {value === 0 && <TabContainer>Item One</TabContainer>}
-      {value === 1 && <TabContainer>Item Two</TabContainer>}
-      {value === 2 && <TabContainer>Item Three</TabContainer>}
-      {value === 3 && <TabContainer>Item Four</TabContainer>}
-      {value === 4 && <TabContainer>Item Five</TabContainer>}
-      {value === 5 && <TabContainer>Item Six</TabContainer>}
-      {value === 6 && <TabContainer>Item Seven</TabContainer>}
-      {value === 7 && <TabContainer>Item Eight</TabContainer>}
-      {value === 8 && <TabContainer>Item Nine</TabContainer>}
-      {value === 9 && <TabContainer>Item Ten</TabContainer>}
-    </div>
+          variant='scrollable'
+          scrollButtons='auto'>
+          <ModuleTab label="Workflows" />
+          <ModuleTab label="Datasets" />
+          <ModuleTab label="Connections" />
+          <ModuleTab label="Workflows" />
+          <ModuleTab label="Datasets" />
+          <ModuleTab label="Connections" />
+          <ModuleTab label="Workflows" />
+          <ModuleTab label="Datasets" />
+          <ModuleTab label="Connections" />
+          <ModuleTab label="Workflows" />
+          <ModuleTab label="Datasets" />
+          <ModuleTab label="Connections" />
+        </ModuleTabs>
+      </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,5 @@ import App from './app';
 
 // Styles
 import './styles/main.css';
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
